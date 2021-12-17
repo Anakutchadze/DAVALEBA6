6b2f4fdaaeaab6bbf4d41f35a551f6c3a4942ff6
ANAKUTCHADZE <ani.kutchadze.1@btu.edu.ge>
2021-12-17 20:45:26 +0400

    HERO

---
+++ app/src/main/java/com/example/myapplication/fragments/FragmentThird.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class FragmentThird: Fragment(R.layout.third_fragment) {
+
+
+}
